db6224b320fdbd94c80b6e3dec45b3812f63fd33
Gunther Bachmann <gunther-bachmann@users.noreply.github.com>
2017-01-31 12:48:50 +0100

    Update isVersionTag.groovy
    Allow for trailing ^0 when pattern matching

--- vars/isVersionTag.groovy
+++ vars/isVersionTag.groovy
@@ -1,5 +1,5 @@
 boolean call() {
-    def versionPattern = /v\d+.\d+(.\d+)?/
+    def versionPattern = /v\d+.\d+(.\d+)?(\^0)?/
     def tag = bash('git name-rev --name-only --tags HEAD').trim()
     return tag ==~ versionPattern
 }
